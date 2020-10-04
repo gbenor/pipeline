e8518196c1de8b29494eb9759e038baae622fca4
DESKTOP-H9PKNVK\user <benorgi@post.bgu.ac.il>
2020-10-04 21:47:48 +0300

    refactor. use a generic fasta reader function

--- mirna_utils/gambiae.py
+++ mirna_utils/gambiae.py
@@ -4,22 +4,23 @@ from pandas import DataFrame
 
 from consts.mirna_utils import GAMBIAE_FILE, MIRBASE_FILE
 from utils.logger import logger
+from utils.utils import fasta_to_dataframe
 
 
 def read_aga_fasta():
     mirbase_df: DataFrame = pd.read_csv(MIRBASE_FILE, index_col=0)
 
     ver = "aga_paper"
-    with GAMBIAE_FILE.open()  as fasta:
-        logger.info(f"read fasta file {GAMBIAE_FILE}")
-        cnt = 0
-        for seq_record in SeqIO.parse(fasta, 'fasta'):  # (generator)
-            if str(seq_record.id).startswith("aga-"):
-                cnt+=1
-                mirbase_df = mirbase_df.append(
-                    pd.Series([ver, seq_record.id, str(seq_record.seq)], index=mirbase_df.columns), ignore_index=True)
+    gambiae_mirna: DataFrame = fasta_to_dataframe(GAMBIAE_FILE, "aga-")
+    gambiae_mirna.insert(0, "ver", ver)
 
-    mirbase_df.to_csv(MIRBASE_FILE)
+    assert list(mirbase_df.columns) == list(gambiae_mirna.columns), \
+        f"Can't concatenate the dataframe since they don't have the same columns name: " \
+        f"{list(mirbase_df.columns)}" \
+        f"{list(gambiae_mirna.columns)}"
+
+    merge_df = pd.concat([mirbase_df, gambiae_mirna], ignore_index=True)
+    merge_df.to_csv(MIRBASE_FILE)
     logger.info("update mirbase file")
     logger.info(f"add {cnt} sequences")
 
